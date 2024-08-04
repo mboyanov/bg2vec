0dba160d20b06a76d78a3cf111efbcb1f0e60a08
Martin Boyanov <mboyanov@gmail.com>
2024-08-04 13:41:41 +0300

    Rework initialize_peft to return the peft_model

--- bg2vec/model.py
+++ bg2vec/model.py
@@ -52,8 +52,7 @@ def initialize_peft(
     peft_model = get_peft_model(model.get_model_for_peft(), config)
     print(f"Model's Lora trainable parameters:")
     peft_model.print_trainable_parameters()
-    model.set_model_for_peft(peft_model)
-    return model
+    return peft_model
 
 
 
@@ -96,3 +95,4 @@ def load_adapted_model(model_args: ModelArguments, adapter_path):
     model.model.load_adapter(adapter_path)
     return model
 
+from llm2vec.loss.utils import load_loss
